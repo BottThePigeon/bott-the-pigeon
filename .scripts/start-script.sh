3536d97187872363bc02278b7376eec01ace85a7
adad-mitch <82591699+adad-mitch@users.noreply.github.com>
2022-02-04 23:44:20 +0000

    start-script kills btp process and restarts for single updated instance

--- .scripts/start-script.sh
+++ .scripts/start-script.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-~/app/bott-the-pigeon > /dev/null 2> /dev/null < /dev/null & # Run Bott-The-Pigeon web-app binary in the background, allowing CodeDeploy to continue.
+
+# This script should kill all running instances (because init.d and CodeDeploy
+# scripts may run multiple instances) and start a single new BTP process.
+# This should ideally be run when the instance is updated with new BTP code.
+
+# Store the pids of any running BTP instances
+btpPids=$(pidof /home/ec2-user/app/bott-the-pigeon)
+
+# Iterate through pids if it's not empty (so we're not trying to kill null)
+if ((${btpPids[@]})); then
+        for each in "${btpPids[@]}"
+        do
+                # Kill process using SIGTERM
+                kill -15 $each
+        done
+fi
+
+# Run a single BTP process in the background, using the production flag.
+/home/ec2-user/app/bott-the-pigeon --prod > /dev/null 2> /dev/null < /dev/null &
